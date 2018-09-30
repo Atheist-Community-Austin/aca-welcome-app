64857e2990009a77b64d60491a7e953d1f49f628
Shaun Hubbard <shaun@splife.com>
2018-09-29 19:25:20 -0500

    Initial Commit

---
+++ ACA Welcome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ACA Welcome
+//
+//  Created by Shaun Hubbard on 9/29/18.
+//  Copyright © 2018 Shaun Hubbard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ ACA WelcomeTests/ACA_WelcomeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACA_WelcomeTests.swift
+//  ACA WelcomeTests
+//
+//  Created by Shaun Hubbard on 9/29/18.
+//  Copyright © 2018 Shaun Hubbard. All rights reserved.
+//
+
+import XCTest
+@testable import ACA_Welcome
+
+class ACA_WelcomeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
